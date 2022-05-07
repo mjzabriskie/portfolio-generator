5c18ad23529353141c3c021097ef40d3a555219e
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-05-07 17:20:49 -0600

    added validation to questions

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
         {
             type: "input",
             name: "name",
-            message: "What is your name?",
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                }else{
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "github",
-            message: "Enter your GitHub Username",
+            message: "Enter your GitHub Username (Required)",
+            validate: gitNameInput => {
+                if(gitNameInput){
+                    return true;
+                }else{
+                    console.log('Please enter your Github Username');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -45,12 +61,28 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectNameInput => {
+                if(projectNameInput){
+                    return true;
+                }else{
+                    console.log('Please enter your project name');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDesc => {
+                if(projectDesc){
+                    return true;
+                }else{
+                    console.log('Please enter a description of the project');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -61,7 +93,15 @@ Add a New Project
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: projectLink => {
+                if(projectLink){
+                    return true;
+                }else{
+                    console.log('Please enter the GitHub link to your project');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
